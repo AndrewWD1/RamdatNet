58ad5189c937e457c63500603350c8151858e9b1
Andrew Doumont <andrew.doumont@gmail.com>
2020-02-15 21:14:01 +0900

    Added a single test and succesfully ran it

---
+++ RamdatNet.Tests/RamdatNet_AddShould.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RamdatNet.Tests
+{
+  [TestClass]
+  public class RamdatNet_AddShould
+  {
+    [TestMethod]
+    public void Add_InputIs1_InputIs2_Return3()
+    {
+      var result = R.Add(1)(2);
+
+      Assert.AreEqual(3, result);
+    }
+  }
+}
