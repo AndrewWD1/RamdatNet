88dcefb5e4b790c73288e5719028c78a4b23e690
Andrew Doumont <andrew.doumont@gmail.com>
2020-02-20 09:43:01 +0900

    adding test for Any

---
+++ RamdatNet.Tests/AnyTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RamdatNet.Tests
+{
+  [TestClass]
+  public class AnyTests
+  {
+    [TestMethod]
+    public void AnyTests_Doc_Code()
+    {
+      int[] list = { 2, 3, 5 };
+      bool a = R.Any<int>(x => x % 2 == 0)(list); //-> true 
+      bool b = R.Any<int>(x => x > 6)(list); //-> false
+
+      Assert.IsTrue(a);
+      Assert.IsFalse(b);
+    }
+  }
+}
