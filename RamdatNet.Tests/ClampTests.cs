ed5d7b3c70b111098effc4959cd179d4a9f3d236
Andrew Doumont <andrew.doumont@gmail.com>
2020-02-21 22:55:22 +0900

    adding tests for Chain and Clamp

---
+++ RamdatNet.Tests/ClampTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class ClampTests
+    {
+        [TestMethod]
+        public void ClampTests_Docs()
+        {
+            Assert.AreEqual(1, R.Clamp(1, 10)(-5));
+            Assert.AreEqual(10, R.Clamp(1, 10)(15));
+            Assert.AreEqual(4, R.Clamp(1, 10)(4));
+        }
+
+    }
+}
