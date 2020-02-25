220a096d1e4a8c492692762754f9865bfe43361f
Andrew Doumont <andrew.doumont@gmail.com>
2020-02-25 09:17:15 +0900

    added docs, string overload and tests for DropLastWhile

---
+++ RamdatNet.Tests/DropLastWhileTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class DropLastWhileTests
+    {
+        [TestMethod]
+        public void DropLastWhileTests_Docs_Enumerable()
+        {
+            Predicate<int> lteThree = x => 3 >= x;
+            var result = R.DropLastWhile(lteThree)(new int[] { 1, 2, 3, 4, 3, 2, 1 });
+            int[] expected = { 1, 2, 3, 4 };
+
+            CollectionAssert.AreEqual(expected, result.ToArray());
+        }
+        [TestMethod]
+        public void DropLastWhileTests_Docs_String()
+        {
+
+            var result = R.DropLastWhile(x => x != 'd')("Ramda");
+            var expected = "Ramd";
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
--- RamdatNet/R.cs
+++ RamdatNet/R.cs
@@ -519,11 +519,19 @@ namespace RamdatNet
         /// Returns a list containing all but the last n elements of the given list.
         /// </summary>
         /// <code>
-        /// R.Drop(3)( "ramda"); //=> "ra"
+        /// R.Drop(3)("ramda"); //=> "ra"
         /// </code>
         public static Func<string, string> DropLast(int i)
             => list => String.Join("", list.Take(list.Count() - i));
 
+        /// <summary>
+        /// Returns a new list excluding all the tailing elements of a given list which satisfy the supplied predicate function. 
+        /// </summary>
+        /// <code>
+        /// Predicate{int} lteThree = (int x) => 3 >= x;
+        /// R.DropLastWhile(lteThree)( new int[] { 1, 2, 3, 4, 3, 2, 1 }); 
+        /// //=> { 1, 2, 3, 4 }
+        /// </code>
         public static Func<IEnumerable<T>, IEnumerable<T>> DropLastWhile<T>(Predicate<T> Fn)
             => list =>
             {
@@ -535,6 +543,25 @@ namespace RamdatNet
                     i -= 1;
                 }
                 return newList;
+            };
+
+        /// <summary>
+        /// Returns a new list excluding all the tailing elements of a given list which satisfy the supplied predicate function. 
+        /// </summary>
+        /// <code>
+        /// R.DropLastWhile(x => x !== "d")("Ramda"); //=> "Ramd"
+        /// </code>
+        public static Func<string, string> DropLastWhile(Predicate<char> Fn)
+            => list =>
+            {
+                var newList = list.Select(x => x).ToList();
+                int i = newList.Count() - 1;
+                while (Fn(newList[i]))
+                {
+                    newList.RemoveAt(i);
+                    i -= 1;
+                }
+                return String.Join("", newList);
             };
 
         public static IEnumerable<T> DropRepeats<T>(IList<T> list)
