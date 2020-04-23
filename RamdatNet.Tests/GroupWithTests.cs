527dab3f89015b3a368b55ebe23ea5b15788e5f0
Andrew Doumont <andrew.doumont@gmail.com>
2020-04-23 12:41:10 +0900

    Added GroupWith and GroupWithTests

---
+++ RamdatNet.Tests/GroupWithTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class GroupWithTests
+    {
+        [TestMethod]
+        public void GroupWithTests_Docs()
+        {
+            var result =
+                R.GroupWith((int x, int y) => x == y)(new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21 });
+
+            var expected = new List<List<int>> {
+                new List<int> { 0 },
+                new List<int> { 1, 1 },
+                new List<int> { 2 },
+                new List<int> { 3 },
+                new List<int> { 5 },
+                new List<int> { 8 },
+                new List<int> { 13 },
+                new List<int> { 21 }
+            };
+            Assert.AreEqual(result.Count(), expected.Count());
+            for (int i = 0; i < result.Count(); i++)
+            {
+                CollectionAssert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
---
+++ RamdatNet/GroupWith.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Curried. Takes a list and returns a list of lists where each sublist's elements are all satisfied pairwise comparison according to the provided function. Only adjacent elements are passed to the comparison function.
+        /// </summary>
+        /// <code>
+        /// R.GroupWith(R.Equals)( { 0, 1, 1, 2, 3, 5, 8, 13, 21 })
+        /// //=> {{0}, {1, 1}, {2}, {3}, {5}, {8}, {13}, {21}}
+        /// </code>
+        public static Func<IList<T>, List<List<T>>> GroupWith<T>(Func<T, T, bool> fn)
+            => list =>
+            {
+                List<List<T>> newList = new List<List<T>>();
+
+                int i = 0;
+                while (i < list.Count())
+                {
+                    List<T> subList = new List<T> { list[i] };
+                    i += 1;
+                    while (i < list.Count() && fn(list[i - 1], list[i]))
+                    {
+                        subList.Add(list[i]);
+                        i += 1;
+                    }
+                    newList.Add(subList);
+                }
+
+                return newList;
+            };
+
+        /// <summary>
+        /// Takes a list and returns a list of lists where each sublist's elements are all satisfied pairwise comparison according to the provided function. Only adjacent elements are passed to the comparison function.
+        /// </summary>
+        /// <code>
+        /// R.GroupWith(R.Equals, { 0, 1, 1, 2, 3, 5, 8, 13, 21 })
+        /// //=> {{0}, {1, 1}, {2}, {3}, {5}, {8}, {13}, {21}}
+        /// </code>
+        public static List<List<T>> GroupWith<T>(Func<T, T, bool> fn, List<T> list)
+        {
+            List<List<T>> newList = new List<List<T>>();
+
+            int i = 0;
+            while (i < list.Count())
+            {
+                List<T> subList = new List<T> { list[i] };
+                i += 1;
+                while (i < list.Count() && fn(list[i - 1], list[i]))
+                {
+                    subList.Add(list[i]);
+                    i += 1;
+                }
+                newList.Add(subList);
+            }
+
+            return newList;
+        }
+    }
+}
