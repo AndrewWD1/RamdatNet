5f658980984f11e9ec3e17b83d253b096a8823af
Andrew Doumont <andrew.doumont@gmail.com>
2020-07-11 10:35:12 -0600

    Added Intersection and IntersectionTests

--- RamdatNet.Tests/IntersectionTests.cs
+++ RamdatNet.Tests/IntersectionTests.cs
@@ -19,5 +19,16 @@ namespace RamdatNet.Tests
             CollectionAssert.AreEqual(arrayT.ToArray(), new int[] { 4, 3 });
         }
 
+        [TestMethod]
+        public void IntersectionTests_Immutability()
+        {
+            var list1 = new HashSet<int> { 1, 2, 3, 4 };
+            var list2 = new HashSet<int> { 7, 6, 5, 4, 3 };
+            var setT = R.Intersection(new HashSet<int> { 1, 2, 3, 4 }, new HashSet<int> { 7, 6, 5, 4, 3 }); //=> { 4, 3 }
+
+            CollectionAssert.AreEqual(list1.ToArray(), new int[] { 1, 2, 3, 4 });
+            CollectionAssert.AreEqual(list2.ToArray(), new int[] { 7, 6, 5, 4, 3 });
+            CollectionAssert.AreEqual(setT.ToArray(), new int[] { 4, 3 });
+        }
     }
 }
--- RamdatNet/Intersection.cs
+++ RamdatNet/Intersection.cs
@@ -6,32 +6,33 @@ namespace RamdatNet
     public partial class R
     {
         /// <summary>
-        /// Inserts the supplied element into the list, at the specified index. Note that this is not destructive: it returns a copy of the list with the changes.  
+        /// Combines two lists into a set (i.e. no duplicates) composed of those elements common to both lists. Note that this is not destructive, and respects the order from the second list or set.
         /// </summary>
         /// <code>
-        /// R.Intersection( new HashSet{int} { 1,2,3,4 }, new HashSet{int} { 7,6,5,4,3 }); //=> { 4, 3 }  
-        /// </code>
-        public static HashSet<T> Intersection<T>(HashSet<T> set1, HashSet<T> set2)
-        {
-            HashSet<T> setCopy = new HashSet<T>(set1);
-
-            setCopy.IntersectWith(set2);
-
-            return setCopy;
-        }
-        /// <summary>
-        /// Inserts the supplied element into the list, at the specified index. Note that this is not destructive: it returns a copy of the list with the changes.  
-        /// </summary>
-        /// <code>
-        /// R.Intersection( new int[] { 1,2,3,4 }, new int[] { 7,6,5,4,3 }); //=> { 4, 3 }  
+        /// R.Intersection( new int[] { 1,2,3,4 }, new int[] { 7,6,5,4,3 }); //=> { 4, 3 }
         /// </code>
         public static IList<T> Intersection<T>(IList<T> set1, IList<T> set2)
         {
-            HashSet<T> setCopy = new HashSet<T>(set1);
+            HashSet<T> setCopy = new HashSet<T>(set2);
 
-            setCopy.IntersectWith(set2);
+            setCopy.IntersectWith(set1);
 
             return setCopy.ToList();
+        }
+
+        /// <summary>
+        /// /// Combines two lists into a set (i.e. no duplicates) composed of those elements common to both lists. Note that this is not destructive, and respects the order from the second list or set.
+        /// </summary>
+        /// <code>
+        /// R.Intersection( new HashSet{int} { 1,2,3,4 }, new HashSet{int} { 7,6,5,4,3 }); //=> { 4, 3 }
+        /// </code>
+        public static HashSet<T> Intersection<T>(HashSet<T> set1, HashSet<T> set2)
+        {
+            HashSet<T> setCopy = new HashSet<T>(set2);
+
+            setCopy.IntersectWith(set1);
+
+            return setCopy;
         }
     }
 }
