5db144bec41687e57a1c893d6d51bf4082410e1f
Andrew Doumont <andrew.doumont@gmail.com>
2020-07-10 19:16:15 -0600

    added InsertAll and InsertAllTests

---
+++ RamdatNet.Tests/InsertAllTests.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class InsertAllTests
+    {
+        [TestMethod]
+        public void InsertAllTests_Docs()
+        {
+            var t = R.InsertAll(2, new int[] { 7, 8, 9 }, new int[] { 1, 2, 3, 4 });
+
+            CollectionAssert.AreEqual(t.ToArray(), new int[] { 1, 2, 7, 8, 9, 3, 4 });
+        }
+    }
+}
--- RamdatNet/InsertAll.cs
+++ RamdatNet/InsertAll.cs
@@ -9,22 +9,23 @@ namespace RamdatNet
         /// Inserts the supplied element into the list, at the specified index. Note that this is not destructive: it returns a copy of the list with the changes.  
         /// </summary>
         /// <code>
-        /// R.Insert(2, 9, new int[] { 1, 2, 3, 4 }); //=> { 1, 2, 9, 3, 4 }
+        /// R.InsertAll(2, new int[] { 7, 8, 9 }, new int[] { 1, 2, 3, 4 }); //=> { 1, 2, 7, 8, 9, 3, 4 }
         /// </code>
-        public static IList<T> Insert<T>(int ind, T t, IList<T> list)
+        public static IList<T> InsertAll<T>(int ind, IList<T> ts, IList<T> list)
         {
             if (ind > list.Count)
                 throw new ArgumentOutOfRangeException();
 
-            T[] arr = new T[list.Count + 1];
-
-            arr[ind] = t;
+            T[] arr = new T[list.Count + ts.Count];
 
             for (int i = 0; i < ind; i++)
                 arr[i] = list[i];
 
+            for (int i = ind; i < ind + ts.Count; i++)
+                arr[i] = ts[i - ind];
+
             for (int i = ind; i < list.Count; i++)
-                arr[i + 1] = list[i];
+                arr[i + ts.Count] = list[i];
 
             return arr;
         }
