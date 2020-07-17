60a830c1241df0f5db382f9446ce8a03fcc19a24
Andrew Doumont <andrew.doumont@gmail.com>
2020-07-16 18:21:53 -0600

    Added MapTests

---
+++ RamdatNet.Tests/MapTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class MapTests
+    {
+        [TestMethod]
+        public void MapTests_Docs()
+        {
+            var result = R.Map((int x) => x * 2)(new int[] { 1, 2, 3 });
+            CollectionAssert.AreEqual(result.ToArray(), new int[] { 2, 4, 6 });
+        }
+    }
+}
