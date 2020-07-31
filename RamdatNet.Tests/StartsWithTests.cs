fc90fcfa5555dee98f4779e01d1ff6a2857cc2b5
Andrew Doumont <andrew.doumont@gmail.com>
2020-07-31 06:23:45 -0600

    Added StartsWith and StartsWithTests. StartsWithTests not implemented yet.

---
+++ RamdatNet.Tests/StartsWithTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    internal class StartsWithTests
+    {
+        [TestMethod]
+        public void StartsWithTests_Docs()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ RamdatNet/StartsWith.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Checks if a list starts with the provided sublist.
+        /// Similarly, checks if a string starts with the provided substring.
+        /// </summary>
+        /// <code>
+        /// R.StartsWith(new char[] { 'a' }, new char[] { 'a', 'b', 'c' }); //=> true
+        /// R.StartsWith(new char[] { 'b' }, new char[] { 'a', 'b', 'c' }); //=> false
+        /// </code>
+        public static Func<IList<T>, bool> StartsWith<T>(IList<T> subList)
+            where T : IComparable
+            => list =>
+            {
+                if (subList.Count > list.Count)
+                    return false;
+
+                for (int i = 0; i < subList.Count; i++)
+                {
+                    if (list[i].CompareTo(subList[i]) != 0) return false;
+                }
+
+                return true;
+            };
+
+        /// <summary>
+        /// Checks if a list starts with the provided sublist.
+        /// Similarly, checks if a string starts with the provided substring.
+        /// </summary>
+        /// <code>
+        /// R.StartsWith("a", "abc"); //=> true
+        /// R.StartsWith("b", "abc");  //=> false
+        /// </code>
+        public static Func<string, bool> StartsWith(string subString)
+            => str => str.StartsWith(subString);
+    }
+}
