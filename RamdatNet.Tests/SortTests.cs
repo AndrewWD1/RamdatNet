9eb73450702d1ac4b7a50caf9f057c1b48aa7ce0
Andrew Doumont <andrew.doumont@gmail.com>
2020-07-30 21:30:54 -0600

    Added Sort and SortTests

---
+++ RamdatNet.Tests/SortTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void SortTests_Docs()
+        {
+            Comparison<int> diff = (a, b) => a - b;
+            CollectionAssert.AreEqual(
+                R.Sort(diff, new int[] { 4, 2, 7, 5 }).ToArray(),
+                new int[] { 2, 4, 5, 7 }
+            );
+        }
+    }
+}
---
+++ RamdatNet/Sort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Returns a copy of the list, sorted according to the comparer function, which should accept two values at a time and return a negative number if the first value is smaller, a positive number if it's larger, and zero if they are equal.
+        /// </summary>
+        /// <code>
+        /// Comparison{int} diff = (a, b) => a - b;
+        /// R.Sort(diff, new int[] { 4, 2, 7, 5 }); //=> { 2, 4, 5, 7 }
+        /// </code>
+
+        public static IEnumerable<T> Sort<T>(Comparison<T> fn, IEnumerable<T> list)
+        {
+            var newList = new List<T>(list);
+            newList.Sort(fn);
+
+            return newList;
+        }
+    }
+}
