b0405f7768a000441ba56c5a1fe627d1d9de247e
Andrew Doumont <andrew.doumont@gmail.com>
2020-08-01 05:50:06 -0600

    Added Tail and TailTests.

---
+++ RamdatNet.Tests/TailTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class TailTests
+    {
+        [TestMethod]
+        public void TailTests_Docs()
+        {
+            CollectionAssert.AreEqual(
+                R.Tail(new int[] { 1, 2, 3 }).ToArray(), new int[] { 2, 3 }
+            );
+            CollectionAssert.AreEqual(
+                R.Tail(new int[] { 1, 2 }).ToArray(), new int[] { 2 }
+            );
+            CollectionAssert.AreEqual(
+            R.Tail(new int[] { 1 }).ToArray(), new int[] { }
+            );
+            CollectionAssert.AreEqual(
+            R.Tail(new int[] { }).ToArray(), new int[] { }
+            );
+            Assert.AreEqual(R.Tail("dabc"), "bc");
+            Assert.AreEqual(R.Tail("ab"), "b");
+            Assert.AreEqual(R.Tail("a"), "");
+            Assert.AreEqual(R.Tail(""), "");
+        }
+    }
+}
--- RamdatNet/Tail.cs
+++ RamdatNet/Tail.cs
@@ -1,10 +1,45 @@
-susing System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RamdatNet
 {
     public partial class R
     {
-        public static IEnumerable<T> Tail<T>(IEnumerable<T> list>)
+        /// <summary>
+        /// Returns all but the first element of the given list or string (or object with a tail method).
+        /// </summary>
+        /// <code>
+        /// R.Tail(new int[] { 1, 2, 3 });  //=> { 2, 3 }
+        /// R.Tail(new int[] { 1, 2 });     //=> { 2 }
+        /// R.Tail(new int[] { 1 });        //=> {  }
+        /// R.Tail(new int[] {  });         //=> {  }
+        /// </code>
+        public static IEnumerable<T> Tail<T>(IEnumerable<T> list)
             => list.Skip(1);
+
+        /// <summary>
+        /// Returns all but the first element of the given list or string (or object with a tail method).
+        /// </summary>
+        /// <code>
+        /// R.Tail(new int[] { 1, 2, 3 });  //=> { 2, 3 }
+        /// R.Tail(new int[] { 1, 2 });     //=> { 2 }
+        /// R.Tail(new int[] { 1 });        //=> {  }
+        /// R.Tail(new int[] {  });         //=> {  }
+        /// </code>
+        public static IList<T> Tail<T>(IList<T> list)
+            => list.Skip(1).ToList();
+
+        /// <summary>
+        /// Returns all but the first element of the given list or string (or object with a tail method).
+        /// </summary>
+        /// <code>
+        /// R.Tail("dabc");  //=> "bc"
+        /// R.Tail("ab");   //=> "b"
+        /// R.Tail("a");    //=> ""
+        /// R.Tail("");     //=> ""
+        /// </code>
+        public static string Tail(string str)
+            => String.Join("", str.Skip(1));
     }
 }
