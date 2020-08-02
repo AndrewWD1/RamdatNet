44e45f04da4a751bd21123143c9d473ef96ec9df
Andrew <andrew.doumont@gmail.com>
2020-08-02 03:34:20 +0000

    Added Zip

---
+++ RamdatNet/Zip.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        public IList<(T, K)> Zip<T, K>(IList<T> listT, IList<K> listK)
+        {
+            
+        }
+    }
+}
