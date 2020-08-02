f0400e860f0830f621f5b696ca0f4d562a1bfbea
Andrew Doumont <andrew.doumont@gmail.com>
2020-08-02 16:35:49 -0600

    Added Without and XOR

---
+++ RamdatNet/Without.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// O(n).
+        /// </summary>
+        /// <code>
+        ///
+        /// </code>
+        public Func<IList<T>, IList<T>> Without<T>(IList<T> drop)
+            => list =>
+            {
+                HashSet<T> set = new HashSet<T>(drop);
+
+                var newList = new List<T>(list);
+
+                int i = 0;
+                while (i < newList.Count())
+                {
+                    if (set.Contains(newList[i]))
+                    {
+                        newList.RemoveAt(i);
+                    }
+                    else
+                        i += 1;
+                }
+
+                return newList;
+            };
+    }
+}
---
+++ RamdatNet/XOR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Exclusive disjunction logical operation. Returns true if one of the arguments is true and the other is false. Otherwise, it returns false.
+        /// </summary>
+        /// <code>
+        /// R.XOR(true, true); //=> false
+        /// R.XOR(true, false); //=> true
+        /// R.XOR(false, true); //=> true
+        /// R.XOR(false, false); //=> false
+        /// </code>
+        public static bool XOR(bool a, bool b) => (a && !b) || (!a && b);
+
+        /// <summary>
+        /// Exclusive disjunction logical operation. Returns true if one of the arguments is true and the other is false. Otherwise, it returns false.
+        /// </summary>
+        /// <code>
+        /// Func{int, bool} func1 = x => x > 0;
+        /// Func{int, bool} func2 = x => x < 0;
+        /// var f = R.XOR(func1, func2);
+        /// f(5) //=> true
+        /// </code>
+        public static Func<T, bool> XOR<T>(Func<T, bool> fn1, Func<T, bool> fn2)
+            => t => XOR(fn1(t), fn2(t));
+    }
+}
