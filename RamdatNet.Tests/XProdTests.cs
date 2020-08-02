f902643e9343baac66f4d7d509d63a84023b085d
Andrew Doumont <andrew.doumont@gmail.com>
2020-08-02 13:27:48 -0600

    Added XProd and XProdTests

---
+++ RamdatNet.Tests/XProdTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class XProdTests
+    {
+        [TestMethod]
+        public void XProdTests_Docs()
+        {
+            CollectionAssert.AreEqual(
+                R.XProd(new int[] { 1, 2 }, new char[] { 'a', 'b' }).ToArray(),
+                new (int, char)[] { (1, 'a'), (1, 'b'), (2, 'a'), (2, 'b') }
+            );
+        }
+    }
+}
---
+++ RamdatNet/XProd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Creates a new list out of the two supplied by creating each possible pair from the lists.
+        /// </summary>
+        /// <code>
+        /// R.XProd(new int[] { 1, 2 }, new char[] { 'a', 'b' }); //=> { (1, 'a'), (1, 'b'), (2, 'a'), (2, 'b') }
+        /// </code>
+        public static IList<(T, K)> XProd<T, K>(IList<T> listT, IList<K> listK)
+        {
+            var list = new List<(T, K)>();
+
+            foreach (T t in listT)
+            {
+                foreach (K k in listK)
+                {
+                    list.Add((t, k));
+                }
+            }
+
+            return list;
+        }
+    }
+}
