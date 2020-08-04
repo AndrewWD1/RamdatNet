6cf6496fcfad80c3110696e2d7a5ad9ba68579da
Andrew Doumont <andrew.doumont@gmail.com>
2020-08-03 20:02:21 -0600

    Added Uniq and UniqTests

---
+++ RamdatNet.Tests/UniqTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet.Tests
+{
+    [TestClass]
+    public class UniqTests
+    {
+        [TestMethod]
+        public void UniqTests_Docs()
+        {
+            CollectionAssert.AreEqual(
+                R.Uniq(new int[] { 1, 1, 2, 1 }).ToArray(),
+                new int[] { 1, 2 }
+            );
+        }
+    }
+}
---
+++ RamdatNet/Uniq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RamdatNet
+{
+    public partial class R
+    {
+        /// <summary>
+        /// Returns a new list containing only one copy of each element in the original list.
+        /// </summary>
+        /// <code>
+        /// R.Uniq(new int[] { 1, 1, 2, 1 }); //=> { 1, 2 }
+        /// </code>
+        public static IList<T> Uniq<T>(IList<T> list)
+            => new HashSet<T>(list).ToList();
+    }
+}
